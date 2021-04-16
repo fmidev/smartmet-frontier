6a9f019f4a8725e7f7897135236b1b267d2b126d
Mika Heiskanen <mika.heiskanen@fmi.fi>
2021-04-16 08:10:55 +0300

    Fixed tests to use /usr/bin if local frontier is not available

--- test/TestRunner.sh
+++ test/TestRunner.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+PATH=..:$PATH
+
 mkdir -p failures
 dots='............................................................'
 
@@ -7,7 +9,7 @@ dots='............................................................'
 
 name="europe-forecast-fi"
 printf "%s %s " $name "${dots:${#name}}"
-../frontier -w woml/europe-forecast.woml -s tpl/europe-forecast.tpl -p stereographic,5,90,60:-12.24349761,31.83310001,74.66294552,54.86671043:458,-1 -d -t conceptualmodelanalysis -l FI-fi > failures/${name}.svg 2>/dev/null
+frontier -w woml/europe-forecast.woml -s tpl/europe-forecast.tpl -p stereographic,5,90,60:-12.24349761,31.83310001,74.66294552,54.86671043:458,-1 -d -t conceptualmodelanalysis -l FI-fi > failures/${name}.svg 2>/dev/null
 result=failures/${name}.svg
 expected=output/${name}.svg
 
@@ -21,7 +23,7 @@ fi
 
 name="europe-forecast-sv"
 printf "%s %s " $name "${dots:${#name}}"
-../frontier -w woml/europe-forecast.woml -s tpl/europe-forecast.tpl -p stereographic,5,90,60:-12.24349761,31.83310001,74.66294552,54.86671043:458,-1 -d -t conceptualmodelanalysis -l SV-se > failures/${name}.svg 2>/dev/null
+frontier -w woml/europe-forecast.woml -s tpl/europe-forecast.tpl -p stereographic,5,90,60:-12.24349761,31.83310001,74.66294552,54.86671043:458,-1 -d -t conceptualmodelanalysis -l SV-se > failures/${name}.svg 2>/dev/null
 result=failures/${name}.svg
 expected=output/${name}.svg
 
